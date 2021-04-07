56a2a554ce45bdda1baa6bfd2b70149491a8a1fb
Nikita Kelevra <nonamerz5@mail.ru>
2021-04-07 22:34:56 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 05.04.2021.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+        
+    @IBOutlet weak var animalResultLabel: UILabel!
+    @IBOutlet weak var aboutUserAnimalLabel: UILabel!
+    
+    var answersChoosen: [Answer] = []
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результат в соответсвии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экране результатов
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    
+    
+    
+    
+    
+}
